8a92d0fd8929861939c1cffc351e5ffd4cbd0c3f
Josh Minnich <joshm@scribd.com>
2021-03-12 19:12:28 -0600

    Remove monoSync

--- .dotfiles/scribd.sh
+++ .dotfiles/scribd.sh
@@ -1,13 +1,3 @@
-syncMonoRepo() {
-  config=~/.devkube/devkube_config_custom.yaml
-
-  if [[ $1 == "disable" ]]; then
-    mv ${config}{,.disabled}
-  else
-    mv ${config}{.disabled,}
-  fi
-}
-
 # Aliases
 alias scribd.git="cd /Users/joshm/Documents/scribd/git/scribd/scribd"
 alias mono.git="cd /Users/joshm/Documents/scribd/git/scribd/npm-packages"
