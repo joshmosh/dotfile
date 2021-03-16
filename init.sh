25491d605829e7311de598ca5f959b760e13b537
Josh Minnich <joshminnich@gmail.com>
2021-03-16 10:21:42 -0500

    Add missing pure theme

--- init.sh
+++ init.sh
@@ -4,9 +4,15 @@ cd $HOME
 
 brew install git
 
+cd $HOME
+mkdir $HOME/.zsh
 cd $HOME/.zsh
 
 curl -o _get https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+
+cd $HOME
+
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
 cd $HOME
 
@@ -20,5 +26,6 @@ ln -s .dotfiles/.zshrc .zshrc
 ln -s .dotfiles/.asdfrc .asdfrc
 ln -s .dotfiles/.editorconfig .editorconfig
 ln -s .dotfiles/.gitignore .gitignore
+ln -s .dotfiles/.gitconfig .gitconfig
 
-source ~/.zshrc
+source $HOME/.zshrc
