834f62f1a60321ee2e0214ef8d8f3574e9a9e6b5
Josh Minnich <joshminnich@gmail.com>
2021-03-23 11:02:53 -0500

    Add SlideShare alias

--- shell/scribd.sh
+++ shell/scribd.sh
@@ -1,5 +1,6 @@
 # Aliases
 alias scribd.git="cd /Users/joshm/Documents/scribd/git/scribd/scribd"
 alias mono.git="cd /Users/joshm/Documents/scribd/git/scribd/npm-packages"
+alias ss.git="cd /Users/joshm/Documents/scribd/git/slideshare-web-app"
 
 alias devkube="dbt devkube"
