b4430b974840bf3f693d36c5dca01aa4659b11d0
Josh Minnich <joshminnich@gmail.com>
2021-04-12 11:54:35 -0500

    Update scribd aliases

--- shell/scribd.sh
+++ shell/scribd.sh
@@ -1,6 +1,8 @@
 # Aliases
-alias scribd.git="cd /Users/joshm/Documents/scribd/git/scribd/scribd"
-alias mono.git="cd /Users/joshm/Documents/scribd/git/scribd/npm-packages"
-alias ss.git="cd /Users/joshm/Documents/scribd/git/slideshare-web-app"
+alias scribd.git="cd ~/Documents/scribd/git/scribd/scribd"
+alias mono.git="cd ~/Documents/scribd/git/scribd/npm-packages"
+
+alias ss.git="cd ~/Documents/scribd/git/slideshare-web-app"
+alias ssds.git="cd ~/Documents/scribd/git/slideshare-design-system"
 
 alias devkube="dbt devkube"
